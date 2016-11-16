f2e86af497572929d57bbeda06532b9d3474b7a2
amy <amy@willnot.date>
2016-11-15 23:10:01 -0500

    More accurate but now sliders kill us to death. Scroll speed 4.

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-CMD='ffmpeg -video_size 322x1078 -framerate 30 -f x11grab -i :0.0+793,0 -acodec libmp3lame -ar 11025 -f mpegts udp://127.0.0.1:1234'
+FPS=60
+echo Running at $FPS FPS...
 
-echo Running $CMD ...
-
-ffmpeg -video_size 322x1078 -framerate 30 -f x11grab -i :0.0+793,0 -acodec libmp3lame -ar 11025 -f mpegts udp://127.0.0.1:1234
+ffmpeg -video_size 360x1080 -framerate $FPS -f x11grab -i :0.0+774,0 -acodec libmp3lame -ar 11025 -f mpegts udp://127.0.0.1:1234
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import imutils
 import cv2
 
 cap = cv2.VideoCapture()
+print('Waiting on video...')
 cap.open('udp://127.0.0.1:1234/')
 if not cap.open:
     print("Not open")
-print('Waiting on video...')
 while True:
     # widthxheight+x+y
     # 322x1078+793+0
@@ -20,6 +20,7 @@ while True:
         im2, cnts, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         for c in cnts:
+            # Holy FUCK do sliders kill us to death @_@
             M = cv2.moments(c)
             epsilon = 0.1 * cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, epsilon, True)
@@ -29,20 +30,22 @@ while True:
                     # Width
                     if abs(70 - abs(approx[0][0][0] - approx[2][0][0])) < 10:
                         ypos = abs(approx[1][0][1])
-                        if ypos > 840 and ypos < 860:
+                        # TODO: Should adjust this based on the last position...
+                        if ypos > 870 and ypos < 890:
                             xpos = abs(approx[0][0][0])
                             # D~10 F~80 J~160 K~240
                             key = 'UNKNOWN'
-                            if abs(10 - xpos) < 5:
+                            if abs(30 - xpos) < 5:
                                 key = 'D'
                                 os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" d')
-                            elif abs(80 - xpos) < 5:
+                            elif abs(100 - xpos) < 5:
                                 key = 'F'
                                 os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" f')
-                            elif abs(160 - xpos) < 5:
+                            elif abs(180 - xpos) < 5:
                                 key = 'J'
                                 os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" j')
-                            elif abs(240 - xpos) < 5:
+                            elif abs(260 - xpos) < 5:
                                 key = 'K'
                                 os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" k')
-                            print('click ' + key)
+                            print('click ' + key + ' @ (' + str(xpos) + ', ' + str(ypos) + ').')
+
