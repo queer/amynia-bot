a470e7eed37787f40299c9187446cee68f92bdfa
amy <amy@willnot.date>
2016-11-17 00:35:57 -0500

    Can now play both 4K and 7K

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -2,4 +2,4 @@
 FPS=60
 echo Running at $FPS FPS...
 
-ffmpeg -video_size 360x1080 -framerate $FPS -f x11grab -i :0.0+774,0 -acodec libmp3lame -ar 11025 -f mpegts udp://127.0.0.1:1234
+ffmpeg -video_size 640x1080 -framerate $FPS -f x11grab -i :0.0+634,0 -acodec libmp3lame -ar 11025 -f mpegts udp://127.0.0.1:1234
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from xdo import Xdo
 import os
 import imutils
 import cv2
@@ -7,11 +8,18 @@ import time
 # We time when each last keypress was, and then we make sure to wait at least 32ms
 # (2 frames @ 60FPS) so that we don't get multiple keypresses
 currentTimeMillis = lambda: int(round(time.time() * 1000))
+sTime = currentTimeMillis()
 dTime = currentTimeMillis()
 fTime = currentTimeMillis()
+spaceTime = currentTimeMillis()
 jTime = currentTimeMillis()
 kTime = currentTimeMillis()
+lTime = currentTimeMillis()
 WAIT_TIME_MILLIS = 80
+xdo = Xdo()
+window_id = xdo.search_windows(winname=bytes('osu!cuttingedge', 'utf-8'))[0]
+
+mode = ''
 
 cap = cv2.VideoCapture()
 print('Waiting on video...')
@@ -42,29 +50,81 @@ while True:
                     if abs(70 - abs(approx[0][0][0] - approx[2][0][0])) < 10:
                         ypos = abs(approx[1][0][1])
                         # TODO: Should adjust this based on the last position...
-                        if ypos > 870 and ypos < 890:
+                        if ypos > 880 and ypos < 895:
+                            #
+                            # 4K mode
+                            #
                             xpos = abs(approx[0][0][0])
                             # D~10 F~80 J~160 K~240
                             key = 'UNKNOWN'
-                            # Try to wait at least 2 frames between keypresses
-                            if abs(30 - xpos) < 5 and abs(dTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
-                                key = 'D'
-                                os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" d')
-                                dTime = currentTimeMillis()
-                            elif abs(100 - xpos) < 5 and abs(fTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
-                                key = 'F'
-                                os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" f')
-                                fTime = currentTimeMillis()
-                            elif abs(180 - xpos) < 5 and abs(jTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
-                                key = 'J'
-                                os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" j')
-                                jTime = currentTimeMillis()
-                            elif abs(260 - xpos) < 5 and abs(kTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
-                                key = 'K'
-                                os.system('xdotool key --window "$(xdotool search --name \'osu!cuttingedge\')" k')
-                                kTime = currentTimeMillis()
+                            if mode == '4K':
+                                # D F J K
+                                if abs(170 - xpos) < 5 and abs(dTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'D'
+                                    xdo.send_keysequence_window(window_id, 'd'.encode('utf-8'))
+                                    dTime = currentTimeMillis()
+                                elif abs(240 - xpos) < 5 and abs(fTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'F'
+                                    xdo.send_keysequence_window(window_id, 'f'.encode('utf-8'))
+                                    fTime = currentTimeMillis()
+                                elif abs(320 - xpos) < 5 and abs(jTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'J'
+                                    xdo.send_keysequence_window(window_id, 'j'.encode('utf-8'))
+                                    jTime = currentTimeMillis()
+                                elif abs(400 - xpos) < 5 and abs(kTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'K'
+                                    xdo.send_keysequence_window(window_id, 'k'.encode('utf-8'))
+                                    kTime = currentTimeMillis()
+                            elif mode == '7K':
+                                # S D F (space) J K L
+                                if abs(56 - xpos) < 5 and abs(sTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'S'
+                                    xdo.send_keysequence_window(window_id, 's'.encode('utf-8'))
+                                    sTime = currentTimeMillis()
+                                if abs(136 - xpos) < 5 and abs(dTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'D'
+                                    xdo.send_keysequence_window(window_id, 'd'.encode('utf-8'))
+                                    dTime = currentTimeMillis()
+                                elif abs(205 - xpos) < 5 and abs(fTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'F'
+                                    xdo.send_keysequence_window(window_id, 'f'.encode('utf-8'))
+                                    fTime = currentTimeMillis()
+                                elif abs(289 - xpos) < 5 and abs(spaceTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = '(space)'
+                                    xdo.send_keysequence_window(window_id, 'space'.encode('utf-8'))
+                                    spaceTime = currentTimeMillis()
+                                elif abs(360 - xpos) < 5 and abs(jTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'J'
+                                    xdo.send_keysequence_window(window_id, 'j'.encode('utf-8'))
+                                    jTime = currentTimeMillis()
+                                elif abs(440 - xpos) < 5 and abs(kTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'K'
+                                    xdo.send_keysequence_window(window_id, 'k'.encode('utf-8'))
+                                    kTime = currentTimeMillis()
+                                elif abs(515 - xpos) < 5 and abs(lTime - currentTimeMillis()) > WAIT_TIME_MILLIS:
+                                    key = 'L'
+                                    xdo.send_keysequence_window(window_id, 'l'.encode('utf-8'))
+                                    fTime = currentTimeMillis()
                             # If we don't know what something is, just outright ignore it and hope for the best
                             if key == 'UNKNOWN':
                                 continue
                             print('click ' + key + ' @ (' + str(xpos) + ', ' + str(ypos) + ').')
+                else:
+                    height = abs(approx[0][0][1] - approx[1][0][1])
+                    if abs(140 - height) < 20:
+                        width = abs(approx[0][0][0] - approx[2][0][0])
+                        xpos = abs(approx[0][0][0])
+                        if abs(56 - xpos) < 5:
+                            if mode != '7K':
+                                mode = '7K'
+                        elif abs(170 - xpos) < 5:
+                            if mode != '4K':
+                                mode = '4K'
+                        #key_size_guess = width / 70.0
+                        #if key_size_guess > 6 and key_size_guess < 8:
+                        #    print('Guessing that 7K')
+                        #elif key_size_guess > 3 and key_size_guess < 5:
+                        #    print('Guessing that 4K')
+                        #else:
+                        #    print('?K: width=' + str(width) + ', key_size_guess=' + str(key_size_guess) + ', xpos=' + str(abs(approx[0][0][0])) + ', ypos=' + str(abs(approx[1][0][1])))
 
